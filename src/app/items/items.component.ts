790164e382e916e21647d99ced6cef9027ec168a
Apex-Code <upstaterounder@gmail.com>
2017-10-27 12:19:47 -0500

    create and update items component

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent implements OnInit {
+export class ItemsComponent {
+title = 'Items available for Sale:';
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+items = ['Beer', 'Cigarettes', 'Good Times'];
 
 }
